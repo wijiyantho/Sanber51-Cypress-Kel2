0eb5bb4ea985770361df53bcd51b3f54f6e413f4
wijiyantho <awijiyantho@gmail.com>
2023-11-27 19:38:50 +0700

    update for Sign Up-In process

--- cypress/e2e/1. WebTest/homepage.cy.js
+++ cypress/e2e/1. WebTest/homepage.cy.js
@@ -4,85 +4,73 @@ describe('Home Page', () => {
     cy.visit('https://magento.softwaretestingboard.com/')
   })
 
-  it('Sign Up Test 1st Time', () => {
+  it('Sign Up with various test case', () => {
 
     cy.get(':nth-child(3)').contains('Create an Account').click()
     cy.location('pathname').should('equal','/customer/account/create/')
     
-    it('Insert empty first name', () => {
-      cy.get('#firstname').should('exist').type('{enter}')
-      cy.get('#firstname-error').should('be.visible')
-    })
+    // Insert empty first name
+    cy.get('#firstname').should('exist').type('{enter}')
+    cy.get('#firstname-error').should('be.visible')
   
     // Insert correct first name
     cy.get('#firstname').type('saya{enter}')
 
-    it('Insert empty last name', () => {
-      cy.get('#lastname').should('exist').type('{enter}')
-      cy.get('#lastname-error').should('be.visible')
-    })
+    // Insert empty last name
+    cy.get('#lastname').should('exist').type('{enter}')
+    cy.get('#lastname-error').should('be.visible')
 
     // Insert correct last name
     cy.get('#lastname').should('exist').type('saya{enter}')
 
-    it('Insert empty email', () => {
-      cy.get('#email_address').should('exist').type('{enter}')
-      cy.get('#email_address-error').should('be.visible')
-    })
+    // Insert empty email
+    cy.get('#email_address').should('exist').type('{enter}')
+    cy.get('#email_address-error').should('be.visible')
 
-    it('Insert wrong email', () => {
-      cy.get('#email_address').should('exist').type('saya{enter}')
-      cy.get('#email_address-error').should('be.visible').contains('Ex: johndoe@domain.com')
-    })
+    // Insert wrong email
+    cy.get('#email_address').should('exist').type('saya{enter}')
+    cy.get('#email_address-error').should('be.visible').contains('Ex: johndoe@domain.com')
 
-    it('Insert correct email', () => {
-      cy.get('#email_address').clear()
-      cy.get('#email_address').should('exist').type('saya@example.com{enter}')
-      cy.get('#password').should('exist')
-    })
+    // Insert correct email
+    cy.get('#email_address').clear()
+    cy.get('#email_address').should('exist').type('saya@example.com{enter}')
+    cy.get('#password').should('exist')
 
-    it('Insert empty password', () => {
-      cy.get('#password').type('{enter}')
-      cy.get('#password-strength-meter').should('be.visible').contains('No Password')
-    })
+    // Insert empty password
+    cy.get('#password').type('{enter}')
+    cy.get('#password-strength-meter').should('be.visible').contains('No Password')
 
-    it('Insert weak password', () => {
-      cy.get('#password').type('saya{enter}')
-      cy.get('#password-strength-meter').should('be.visible').contains('Weak')
-    })
+    // Insert weak password
+    cy.get('#password').type('saya{enter}')
+    cy.get('#password-strength-meter').should('be.visible').contains('Weak')
 
-    it('Insert correct password', () => {
-      cy.get('#password').clear()
-      cy.get('#password').type('Saya@1234{enter}')
-      cy.get('#password-strength-meter').should('exist')
-    })
+    // Insert correct password
+    cy.get('#password').clear()
+    cy.get('#password').type('Saya@1234{enter}')
+    cy.get('#password-strength-meter').should('exist')
 
-    it('Confirm different password', () => {
-      cy.get('#password-confirmation').type('Saya@123456{enter}')
-      cy.get('#password-confirmation-error').should('be.visible')
-    })
+    // Confirm different password
+    cy.get('#password-confirmation').type('Saya@123456{enter}')
+    cy.get('#password-confirmation-error').should('be.visible')
 
-    it('Confirm same password', () => {
-      cy.get('#password-confirmation').clear().type('Saya@1234{enter}')
-      cy.location().should((loc) => {expect(loc.href).to.include('/customer/account/')})
-    })
+    // Confirm same password
+    cy.get('#password-confirmation').clear().type('Saya@1234{enter}')
+    cy.location().should((loc) => {expect(loc.href).to.include('/customer/account/')})
 
   })
 
-  it('Sign In Test', ()=> {
+  it('Sign In with various test case', ()=> {
     cy.get('.authorization-link').contains('Sign In').click()
     cy.location().should((loc) => {expect(loc.href).to.include('customer/account/login')})
 
-    it('input wrong e-mail and password', () => {
-      cy.get('#email').type('saya@example.com')
-      cy.get('#pass').type('saya@123456{enter}')
-      cy.get('.message-error').should('be.visible')
-    })
+    // input wrong e-mail and password
+    cy.get('#email').type('saya@example.com')
+    cy.get('#pass').type('saya@123456{enter}')
+    cy.get('.message-error').should('be.visible')
 
-    it('input correct e-mail and password', () => {
-      cy.get('#email').clear().type('saya@contoh.com')
-      cy.get('#pass').type('saya@1234{enter}')
-    })
+    // input correct e-mail and password
+    cy.get('#email').clear().type('saya@contoh.com')
+    cy.get('#pass').type('saya@1234{enter}')
 
   })
 
